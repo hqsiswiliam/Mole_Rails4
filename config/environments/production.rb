bdf89167803abeb050197b1ac0356494239f7bbb
hqsiswiliam <hqsiswiliam@gmail.com>
2013-11-28 05:28:48 +0000

    Add Procfile to launch Puma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ MolePostgres::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.preload_frameworks = true
+  config.allow_concurrency = true
 end
